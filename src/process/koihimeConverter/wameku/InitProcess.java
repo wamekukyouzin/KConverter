8654ea705f8b2bf1b324fafb953d08b5c7ab6351
wamekukyouzin <wamekukyouzin@gmail.com>
2017-03-13 13:58:30 +0900

    命蓮を追加。

--- src/process/koihimeConverter/wameku/InitProcess.java
+++ src/process/koihimeConverter/wameku/InitProcess.java
@@ -12,7 +12,7 @@ public class InitProcess extends Process {
 		//霖之助と妖忌に「オトコ」を、それ以外に女を設定する。
 		//もう名前で振り分けたんでいいや。
 		String firstName = data.get("名前");
-		if (firstName.equals("霖之助") || firstName.equals("妖忌") || firstName.equals("雲山") || firstName.equals("玄爺") || firstName.equals("月の門番") || firstName.equals("易者"))
+		if (firstName.equals("霖之助") || firstName.equals("妖忌") || firstName.equals("雲山") || firstName.equals("玄爺") || firstName.equals("月の門番") || firstName.equals("易者") || firstName.equals("命蓮"))
 			chara.getTalentMap().put("性別", "0");
 		else {
 			chara.getTalentMap().put("性別", "1");
